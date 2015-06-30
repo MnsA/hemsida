0ca8a73bfbb5fce030de33290dc4dbbdaf48f28b
MnsA <manala@edu.umea.se>
2015-06-30 10:26:56 +0200

    flyttade text till separat fil

--- script/display.js
+++ script/display.js
@@ -74,18 +74,4 @@ function getColor(value) {
 
 var textCube = new TextCube([40, 10, 100, 50]);
 // temp, wind, clouds, rain
-textCube.addPoints([
-	[0, 10, 60, 0], "Kallt och blåsigt.",
-	[20, 5, 0, 0], "Varmt och stilla.",
-	[10, 10, 100, 20], "Regnigt och hemskt.",
-	[20, 1, 15, 0], "Mycket fint väder.",
-	[30, 1, 0, 0], "Gå ut!",
-	[-10, 0, 100, 5], "Snö och sånt.",
-	[-20, 10, 0, 0], "Iskallt och blåsigt.",
-	[-30, 3, 0, 0], "Stanna inomhus om du inte är en norrlänning.",
-	[20, 10, 40, 0], "Varmt och blåsigt.",
-	[10, 1, 60, 0], "Ganska fint väder.",
-	[10, 1, 100, 3], "Lite regnigt.",
-	[20, 1, 80, 4], "Varmt och lite regnigt.",
-	[15, 3, 60, 0], "Ganska varmt.",
-]);
+textCube.addPoints(weatherTextList);
