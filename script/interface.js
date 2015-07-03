e28fe49e0fc4b81b307b5c941113264cd772d3ed
MnsA <manala@edu.umea.se>
2015-07-03 10:36:29 +0200

    extra säker på att den använder cache

--- script/interface.js
+++ script/interface.js
@@ -1,7 +1,8 @@
 function loadWeatherData(callback) {
 	var url = 'http://opendata-download-metfcst.smhi.se/api/category/pmp1.5g/version/1/geopoint/lat/{1}/lon/{2}/data.json';
 	var lat = 63.8, lon = 20.3;
-	// is cached by default
+
+	$.ajaxSetup({cache: true});
 	var req = $.getJSON(url.replace('{1}', lat).replace('{2}', lon));
 	req.done(callback);
 }
